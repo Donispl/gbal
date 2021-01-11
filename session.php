78f4d3257c8113efd01742d52aaaf8f21c36417a
Stephane PLANCY <s.plancy@laposte.net>
2021-01-11 19:09:08 +0100

    ajout des pages du projet

---
+++ session.php
@@ -0,0 +1,39 @@
+< !DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ceci est une page HTML de test</title>
+    </head>
+    <body>
+     <?php 
+
+//  Récupération de l'utilisateur et de son pass hashé
+$req = $bdd->prepare('SELECT id, pass FROM membres WHERE pseudo = :pseudo');
+$req->execute(array(
+    'pseudo' => $pseudo));
+$resultat = $req->fetch();
+
+// Comparaison du pass envoyé via le formulaire avec la base
+$isPasswordCorrect = password_verify($_POST['pass'], $resultat['pass']);
+
+if (!$resultat)
+{
+    echo 'Mauvais identifiant ou mot de passe !';
+}
+else
+{
+    if ($isPasswordCorrect) {
+        session_start();
+        $_SESSION['id'] = $resultat['id'];
+        $_SESSION['pseudo'] = $pseudo;
+        echo 'Vous êtes connecté !';
+    }
+    else {
+        echo 'Mauvais identifiant ou mot de passe !';
+    }
+}
+?>
+    </body>
+</html>
+
+
